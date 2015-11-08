1698d06283055566073dc6d1830dd6e023cc786a
GCLancaster <g.lancaster.93@gmail.com>
2015-11-08 11:28:47 +0000

    Adding gLoopTimer
    Class that handles a "game loop". You can specify an external master fps
    (e.g. 30fps max), and blocks of internal loops (e.g. a 120fps and a
    10fps internals). Functions can be inserted before the internal loop,
    n-count of internal functions, and after the internal functions.

---
+++ gEngineTest/gEngineTest_LoopTiming/gEngineTest_LoopTiming/main.cpp
@@ -0,0 +1,36 @@
+
+#include "gEngine\System\gLoopTimer.h"
+#include <array>
+#undef main
+
+
+int main(int argc, char* args[])
+{
+	GENG::gLoopTimer timer;
+	bool bQuit = false;
+
+	timer.init
+		(
+		[&]() -> void {
+			//DBG("Preloop");
+		},
+		{
+			GENG::gLoopTimer::InternalBlock(30, "InternalLoop 1", [&]() -> void {
+				//std::this_thread::sleep_for(std::chrono::microseconds(rand() % 10));
+				//DBG("Internal");
+			}),
+			GENG::gLoopTimer::InternalBlock(120, "InternalLoop 2", [&]() -> void {
+				//std::this_thread::sleep_for(std::chrono::microseconds(rand() % 10));
+				//DBG("Internal");
+			})
+		},
+		[&]() -> void {
+				//DBG("Postloop");
+		}
+	);
+
+	while (!bQuit)
+	{
+		timer.loop();
+	}
+}
